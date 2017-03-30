02f57bc12851ae0322d081230c99a8ffa7e67ca3
peter.parys <parys@uniquemail.net>
2017-03-30 15:29:56 +0200

    immutable implemented

--- swen2-decorator/src/main/java/decorator/Patient.java
+++ swen2-decorator/src/main/java/decorator/Patient.java
@@ -3,42 +3,33 @@ package decorator;
 import java.util.Date;
 
 public class Patient {
-	
+
 	private int id;
 	private String nachname;
 	private String vorname;
 	private Date gebDat;
-	
-	public Patient(int id) {
+
+	public Patient(int id, String nachname, String vorname, Date gebDat) {
 		this.id = id;
+		this.nachname = nachname;
+		this.vorname = vorname;
+		this.gebDat = gebDat;
 	}
 
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getNachname() {
 		return nachname;
-	}
-
-	public void setNachname(String nachname) {
-		this.nachname = nachname;
 	}
 
 	public String getVorname() {
 		return vorname;
 	}
 
-	public void setVorname(String vorname) {
-		this.vorname = vorname;
-	}
-
 	public Date getGebDat() {
 		return gebDat;
 	}
-
-	public void setGebDat(Date gebDat) {
-		this.gebDat = gebDat;
-	}	
 
 }
