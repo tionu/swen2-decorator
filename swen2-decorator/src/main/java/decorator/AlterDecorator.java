391b0b2ee84c72e53fa10d31f0594b59ad775f18
peter.parys <parys@uniquemail.net>
2017-04-06 14:53:15 +0200

    age calculation implemented

--- swen2-decorator/src/main/java/decorator/AlterDecorator.java
+++ swen2-decorator/src/main/java/decorator/AlterDecorator.java
@@ -1,8 +1,14 @@
 package decorator;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 public class AlterDecorator extends PropertyDecorator {
+
+	private static final DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyy");
 
 	public AlterDecorator(Properties properties) {
 		super(properties);
@@ -10,9 +16,25 @@ public class AlterDecorator extends PropertyDecorator {
 
 	@Override
 	public Map<String, String> getProperties() {
+		Date birthday = null;
+		try {
+			birthday = dateFormat.parse(properties.getProperties().get("Geburtsdatum"));
+		} catch (ParseException ignored) {
+		}
+		if (birthday == null) {
+			throw new IllegalArgumentException("\"Geburtsdatum\" not present in properties.");
+		}
+
 		Map<String, String> addProperty = properties.getProperties();
-		addProperty.put("Alter", "21.03.1994");
+		addProperty.put("Alter", String.valueOf(getDiffYears(birthday)));
 		return addProperty;
+	}
+	
+	private int getDiffYears(Date date){
+	    DateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+	    int past = Integer.parseInt(formatter.format(date));
+	    int now = Integer.parseInt(formatter.format(new Date()));
+	    return (now-past)/10000;
 	}
 
 }
--- swen2-decorator/src/main/java/decorator/Starter.java
+++ swen2-decorator/src/main/java/decorator/Starter.java
@@ -1,12 +1,16 @@
 package decorator;
 
-import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Starter {
+	private static final DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyy");
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+
 		Patient patient = new Patient.PatientBuilder().nachname("Morta").vorname("Lui")
-				.adresse("Spielweg", "5a", 78462, "Konstanz").gebDat(new Date()).build();
+				.adresse("Spielweg", "5a", 78462, "Konstanz").gebDat(dateFormat.parse("21.04.1992")).build();
 		Properties properties = new AlterDecorator(new PatientProperties(patient));
 
 		PropertyViewer propertyViewer = new PropertyViewer(properties);
